29b1b3b3c6492a71833741273cc498b35b87861e
Jack Doyle <nycjackdoyle@gmail.com>
2022-07-11 21:03:08 -0400

    Scrumdinger (Apple sample program)

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Jack Doyle on 7/11/22.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack (alignment: .leading) {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack (alignment: .trailing){
+                    Text("Seconds Remaining")
+                    Label("500", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jack Doyle on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
